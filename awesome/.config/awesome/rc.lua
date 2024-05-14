dfbd0fe602db8bc4962297523d7294d98c2197ad
javlocan <javlocan.dev@gmail.com>
2024-05-14 08:24:53 +0200

    error handling refactored

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -3,9 +3,11 @@ pcall(require, 'luarocks.loader')
 local gears = require 'gears'
 local awful = require 'awful'
 require 'awful.autofocus'
+
 local wibox = require 'wibox'
 local beautiful = require 'beautiful'
 local naughty = require 'naughty'
+
 local lain = require 'lain'
 --local menubar       = require("menubar")
 local freedesktop = require 'freedesktop'
@@ -14,39 +16,8 @@ require 'awful.hotkeys_popup.keys'
 
 local mytable = awful.util.table or gears.table -- 4.{0,1} compatibility
 
-if awesome.startup_errors then
-  naughty.notify {
-    preset = naughty.config.presets.critical,
-    title = 'Oops, there were errors during startup!',
-    text = awesome.startup_errors,
-  }
-end
+dofile(os.getenv 'HOME' .. '/.config/awesome/error_handling.lua')
 
-do
-  local in_error = false
-
-  awesome.connect_signal('debug::error', function(err)
-    if in_error then
-      return
-    end
-
-    in_error = true
-
-    naughty.notify {
-      preset = naughty.config.presets.critical,
-      title = 'Oops, an error happened!',
-      text = tostring(err),
-    }
-
-    in_error = false
-  end)
-end
-
--- }}}
-
--- {{{ Autostart windowless processes
-
--- This function will run once every time Awesome is started
 local function run_once(cmd_arr)
   for _, cmd in ipairs(cmd_arr) do
     awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
