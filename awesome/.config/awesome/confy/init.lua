d0412bcbf66b52f999b69bd19f744de90bef9a9e
javlocan <javlocan.dev@gmail.com>
2024-05-20 23:06:31 +0200

    init clean af and G.client not possible

--- awesome/.config/awesome/confy/init.lua
+++ awesome/.config/awesome/confy/init.lua
@@ -1,27 +1,26 @@
--- confy reexports some variables with the pourpose in mind
--- of making dependencies transparent
-
+-----------------------------------------------------------------------
+------ ░█▀▀▀ ░█▄─░█ 　 ░█▀▄▀█ ─█▀▀█ ▀█▀ ░█▄─░█ ▄▀ ▀▄
+------ ░█▀▀▀ ░█░█░█ 　 ░█░█░█ ░█▄▄█ ░█─ ░█░█░█ █─ ─█
+------ ░█─── ░█──▀█ 　 ░█──░█ ░█─░█ ▄█▄ ░█──▀█ ▀▄ ▄▀-
+-----------------------------------------------------------------------
 local confy = require 'confy.configuration'
 local clients = require 'confy.clients'
 local global = require 'confy.global'
 local screens = require 'confy.screens'
 local theme = require 'confy.theme'
 local signals = require 'confy.signals'
-
+-----------------------------------------------------------------------
+---   awesomewm defines a global context which includes this variables
 ---@diagnostic disable-next-line: undefined-global, unused-local
-local awesome, root = awesome, root
----
-
-G = {
-  client = client,
-  screen = screen,
-}
-
+local awesome, root, screen, client = awesome, root, screen, client
+-----------------------------------------------------------------------
+---  confy functions as a re-export barrel for the purpose of
+---  1. making dependency flow more transparent between modules
+---  2. turning this file into a relevant one for quick tinkerings
+-----------------------------------------------------------------------
 confy:autostart()
-
 screens:set() -- se va pa screen
-theme:set { bar = true }
-
+theme:set { bar = true, menu = true }
 global:set { keys = confy.keys.global, buttons = confy.buttons.global }
 clients:set { keys = confy.keys.client, buttons = confy.buttons.client }
 
@@ -31,3 +30,5 @@ clients:set { keys = confy.keys.client, buttons = confy.buttons.client }
 -- sumtin like that
 
 signals:connect()
+
+-----------------------------------------------------------------------
