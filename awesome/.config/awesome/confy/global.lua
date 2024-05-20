b0bfdb0630c467fbf87f1e9d98b2485273e04fc7
javlocan <javlocan.dev@gmail.com>
2024-05-20 08:57:09 +0200

    Refactor going on

--- awesome/.config/awesome/confy/global.lua
+++ awesome/.config/awesome/confy/global.lua
@@ -1,8 +1,14 @@
+local awful = require 'awful'
+
 local keys = require 'confy.keys'
+
+local const = require 'confy.configuration.const'
 
 local M = {}
 
 M.set = function(_, globalkeys, globalbuttons)
+  awful.util.terminal = const.misc.terminal
+
   globalkeys = keys.build_global_tag_navigation(globalkeys)
   root.keys = globalkeys
   root.buttons = globalbuttons
